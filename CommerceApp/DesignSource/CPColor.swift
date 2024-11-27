1f578a8b78edab652467243937e4ede5867de7ce
jeoungsung12 <jeoungsung12@naver.com>
2024-11-27 20:39:31 +0900

    디자인 리소스 컴포넌트화

---
+++ CommerceApp/DesignSource/CPColor.swift
@@ -0,0 +1,20 @@
+//
+//  CPColor.swift
+//  CommerceApp
+//
+//  Created by 정성윤 on 11/27/24.
+//
+
+import Foundation
+import UIKit
+
+enum CPColor {
+    static var bk: UIColor = UIColor(named: "bk")!
+    static var wh: UIColor = UIColor(named: "wh")!
+    static var coral: UIColor = UIColor(named: "coral")!
+    static var yellow: UIColor = UIColor(named: "yellow")!
+    static var keyColorRed: UIColor = UIColor(named: "key-color-red")!
+    static var keyColorRed2: UIColor = UIColor(named: "key-color-red2")!
+    static var keyColorBlue: UIColor = UIColor(named: "key-color-blue")!
+    static var keyColorBlueTrans: UIColor = UIColor(named: "key-color-blue-trans")!
+}
