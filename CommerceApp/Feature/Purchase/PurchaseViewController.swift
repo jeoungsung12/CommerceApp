2a14e687341f099c5ce19808546d0d70a2504d8b
jeoungsung12 <jeoungsung12@naver.com>
2024-12-03 15:40:52 +0900

    스크롤뷰를 이용하여 결제 화면 구현

--- CommerceApp/Feature/Purchase/PurchaseViewController.swift
+++ CommerceApp/Feature/Purchase/PurchaseViewController.swift
@@ -6,8 +6,15 @@
 //
 
 import UIKit
+import Combine
 
 final class PurchaseViewController: UIViewController {
+    private var viewModel: PurchaseViewModel = PurchaseViewModel()
+    private var scrollViewConstraints: [NSLayoutConstraint]?
+    private var titleLabelConstraints: [NSLayoutConstraint]?
+    private var purchaseItemStackViewConstraints: [NSLayoutConstraint]?
+    private var purchaseButtonConstraints: [NSLayoutConstraint]?
+    private var subscriptions = Set<AnyCancellable>()
     
     private var scrollView: UIScrollView = {
         let view: UIScrollView = UIScrollView()
@@ -30,35 +37,110 @@ final class PurchaseViewController: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-
+    
+    private var purchaseItemStackView: UIStackView = {
+        let view: UIStackView = UIStackView()
+        view.axis = .vertical
+        view.alignment = .fill
+        view.distribution = .fill
+        view.spacing = 7
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private var purchaseButton: UIButton = {
+        let btn: UIButton = UIButton()
+        btn.setTitle("결제하기", for: .normal)
+        btn.titleLabel?.font = CPFont.m16
+        btn.layer.backgroundColor = CPColor.UIKit.keyColorBlue.cgColor
+        btn.layer.cornerRadius = 5
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        return btn
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .white
+        addSubView()
+        bindViewModel()
+        viewModel.process(.loadData)
+    }
+    
+    override func updateViewConstraints() {
+        if scrollViewConstraints == nil {
+            let constraints = [
+                scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+                scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+                scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+                scrollView.bottomAnchor.constraint(equalTo: purchaseButton.topAnchor),
+                
+                containerView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+                containerView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+                containerView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+                containerView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+                containerView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+            ]
+            
+            NSLayoutConstraint.activate(constraints)
+            scrollViewConstraints = constraints
+        }
+        
+        if titleLabelConstraints == nil,
+           let superView = titleLabel.superview {
+            let constraints = [
+                titleLabel.topAnchor.constraint(equalTo: superView.topAnchor, constant: 33),
+                titleLabel.leadingAnchor.constraint(equalTo: superView.leadingAnchor, constant: 33),
+                titleLabel.trailingAnchor.constraint(equalTo: superView.trailingAnchor, constant: -33)
+            ]
+            NSLayoutConstraint.activate(constraints)
+            titleLabelConstraints = constraints
+        }
+        
+        if purchaseItemStackViewConstraints == nil {
+            let constraints = [
+                purchaseItemStackView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 19),
+                purchaseItemStackView.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor, constant: 20),
+                purchaseItemStackView.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor, constant: -20),
+                purchaseItemStackView.bottomAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 33)
+            ]
+            NSLayoutConstraint.activate(constraints)
+            purchaseItemStackViewConstraints = constraints
+        }
+        
+        if purchaseButtonConstraints == nil {
+            let constraints = [
+                purchaseButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+                purchaseButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+                purchaseButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -32),
+                purchaseButton.heightAnchor.constraint(equalToConstant: 50)
+            ]
+            NSLayoutConstraint.activate(constraints)
+            titleLabelConstraints = constraints
+        }
+        super.updateViewConstraints()
+    }
+    
+    private func addSubView() {
         view.addSubview(scrollView)
         scrollView.addSubview(containerView)
         containerView.addSubview(titleLabel)
-        
-        NSLayoutConstraint.activate([
-            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        ])
-        
-        NSLayoutConstraint.activate([
-            containerView.topAnchor.constraint(equalTo: scrollView.topAnchor),
-            containerView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-            containerView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-            containerView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor)
-        ])
-        
-        NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 33),
-            titleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 33),
-            titleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -33)
-        ])
+        containerView.addSubview(purchaseItemStackView)
+        view.addSubview(purchaseButton)
     }
     
+    private func bindViewModel() {
+        viewModel.$state
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] _ in
+                self?.purchaseItemStackView.subviews.forEach {
+                    $0.removeFromSuperview()
+                }
+                self?.viewModel.state.purchaseItems?.forEach {
+                    self?.purchaseItemStackView.addArrangedSubview(PurchaseSelectedItemView(viewModel: .init(title: $0.title, description: $0.description)))
+                }
+            }
+            .store(in: &subscriptions)
+    }
 }
 
 #Preview {
--- CommerceApp/Feature/Purchase/PurchaseViewModel.swift
+++ CommerceApp/Feature/Purchase/PurchaseViewModel.swift
@@ -8,6 +8,35 @@
 import Foundation
 import Combine
 
-final class PurchaseViewModel {
+final class PurchaseViewModel: ObservableObject {
+    enum Action {
+        case loadData
+        case didTapPurchaseButton
+    }
+    struct State {
+        var purchaseItems: [PurchaseSelectedItemViewModel]?
+    }
+    @Published private(set) var state: State = State()
     
+    func process(_ action: Action) {
+        switch action {
+        case .loadData:
+            Task { await loadData() }
+        case .didTapPurchaseButton:
+            Task { await didTapPurchaseButton() }
+        }
+    }
+    
+}
+
+extension PurchaseViewModel {
+    @MainActor
+    private func loadData() async {
+        
+    }
+    
+    @MainActor
+    private func didTapPurchaseButton() async {
+        
+    }
 }
